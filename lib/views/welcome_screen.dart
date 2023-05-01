256f4f90aa31af932110f632f175609f54c22b0c
Tuan Plus <Dongoctuan2002kk@gmail.com>
2023-05-01 14:30:44 +0700

    🎨 style(main.dart): change HomePage to WelcomePage 🚀 feat(welcome_screen.dart): add welcome screen with image and button to navigate to HomePage 📦 chore(pubspec.yaml): add google_fonts package to dependencies 📦 chore(pubspec.yaml): add assets directory to assets section in flutter configuration

---
+++ lib/views/welcome_screen.dart
@@ -0,0 +1,97 @@
+import 'package:covid19_tracker/views/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            margin: EdgeInsets.only(
+                bottom: MediaQuery.of(context).size.height * 0.3),
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/welcome.png'),
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+          Container(
+            margin:
+                EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.65),
+            width: double.infinity,
+            decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(40))),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(left: 30, top: 30),
+                  child: Text(
+                    'Be Aware\nStay Healthy',
+                    style: GoogleFonts.questrial(
+                      fontSize: 38,
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(left: 30, top: 30),
+                  child: Text(
+                    'Welcome to COVID-19 information portal.',
+                    style: GoogleFonts.poppins(
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(
+                      left: MediaQuery.of(context).size.width * 0.55, top: 40),
+                  alignment: Alignment.bottomRight,
+                  child: Row(
+                    children: [
+                      Text(
+                        'GET STARTED',
+                        style: GoogleFonts.poppins(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Container(
+                        decoration: const BoxDecoration(
+                          color: Colors.purple,
+                          borderRadius: BorderRadius.all(Radius.circular(45)),
+                        ),
+                        child: IconButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const HomePage()));
+                          },
+                          icon: const Icon(
+                            Icons.arrow_forward,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
